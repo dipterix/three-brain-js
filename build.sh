7b6ad44202ab2e26dc172e82715d65c862f9c0a4
dipterix <dipterix.wang@gmail.com>
2023-07-14 16:17:53 -0400

    Fixing build: remove self brewed lil-gui

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # copy branch of lil-gui with modification
-if [[ ! -d "node_modules/lil-gui" ]]
-then
-  echo "lil-gui is missing"
-  if [[ ! -d "lil-gui/dist" ]]
-  then
-    echo "Building home-brewed version of lil-gui"
-    rm -rf lil-gui
-    git clone https://github.com/dipterix/lil-gui.git
-    cd lil-gui
-    npm install
-    npm run build
-    cd ../
-  fi
-  cp -r lil-gui node_modules/
-  rm -rf lil-gui/
-fi
+# if [[ ! -d "node_modules/lil-gui" ]]
+# then
+#   echo "lil-gui is missing"
+#   if [[ ! -d "lil-gui/dist" ]]
+#   then
+#     echo "Building home-brewed version of lil-gui"
+#     rm -rf lil-gui
+#     git clone https://github.com/dipterix/lil-gui.git
+#     cd lil-gui
+#     npm install
+#     npm run build
+#     cd ../
+#   fi
+#   cp -r lil-gui node_modules/
+#   rm -rf lil-gui/
+# fi
 
 # cd inst/js_raws
 npx webpack
