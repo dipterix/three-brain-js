619e91265b358e3ae2603e3f4d91956ac78801ca
dipterix <dipterix.wang@gmail.com>
2026-09-14 16:47:01 -0400

    Removed stray `WebGPU` imports

--- src/js/core/ViewerCanvas.js
+++ src/js/core/ViewerCanvas.js
@@ -10,7 +10,6 @@ import {
   SphereGeometry, BufferGeometry, MeshBasicMaterial,
   LineBasicMaterial, LineSegments
 } from 'three';
-import { WebGPURenderer } from 'three/webgpu';
 import Stats from 'stats-gl';
 import { json2csv } from 'json-2-csv';
 import download from 'downloadjs';
